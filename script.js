2594a67d0e0137c7bb51f5e145a389481fe264bd
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-10-27 11:07:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,12 @@ function volume_sphere() {
 		 
 	  const radius = parseFloat(document.getElementById('radius').value);
 	
-	 if(radius>0)
-	 {
+	
 		 const volume = (4/3) * Math.PI * Math.pow(radius, 3);
-        // alert(volume);
+        
 		 document.getElementById('volume').value = volume;
-	 }
-	else
-	 {
-		 document.getElementById('volume').value = "Invalid Input";
-	 }
+	 
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
